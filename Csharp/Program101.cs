4d4aace70132a50e758ae8f5a80ff3693b82b6cc
Michael Murphy <10904988+murfrosoft@users.noreply.github.com>
2020-02-26 12:37:20 -0600

    C# example

---
+++ Csharp/Program101.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program101
+{
+    class Program
+    {
+        static int IntPrompt(string text)
+        {
+            bool goodParse = false;
+            int result = 0;
+            while (!goodParse)
+            {
+                try
+                {
+                    Console.Write(text);
+                    result = Convert.ToInt32(Console.ReadLine());
+                    goodParse = true;
+                }
+                catch
+                {
+                    Console.WriteLine("*Invalid input. Try again*");
+                }
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            // Prompt for grades
+            int count = IntPrompt("How many grades will you enter: ");
+
+            while (count < 3)
+            {
+                Console.WriteLine("*Minimum value of 3 expected*");
+                count = IntPrompt("How many grades will you enter: ");
+            }
+
+            int[] grades = new int[count];
+            Console.WriteLine();
+
+            int minGrade = int.MaxValue;
+            int minIndex = 0;
+            int sumGrade = 0;
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                grades[i] = IntPrompt($"Enter grade {i + 1}: ");
+                sumGrade += grades[i];
+                if (grades[i] < minGrade)
+                {
+                    minGrade = grades[i];
+                    minIndex = i;
+                }
+            }
+
+            // Print entered grades:
+            Console.WriteLine("\nYour grades:");
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                if (i == minIndex)
+                {
+                    Console.WriteLine("{0} *dropped*", grades[i]);
+                }
+                else
+                {
+                    Console.WriteLine("{0}", grades[i]);
+                }
+            }
+
+            Console.WriteLine("\nAverage of grades is {0:F}", (sumGrade - minGrade) / (double)(grades.Length - 1));
+
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
